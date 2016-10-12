9273e46fe97e6cca8cd4ebd16ceeb1e2328a1a64
m85091081 <m85091081@gmail.com>
2016-10-12 22:01:06 +0800

    add api mode for login

--- core/user.py
+++ core/user.py
@@ -27,6 +27,25 @@ def login():
     else:
         return redirect(url_for('main.index'))
 
+@auth.route('/api', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['buser']
+        passd = request.form['bpass']
+        password = User.login(user)
+        passd = hashlib.sha256(passd.replace('\n', '').encode()).hexdigest()
+        
+        if not password:
+            return 'no account'
+        else:
+            if password == passd:
+                session['username'] = user
+                session['name'] = user
+                return 'login'
+            else:
+                return 'password err'
+
+
 
 @app.route('/logout')
 def logout():
