013ce1dc37b31b11a9bf6ac22f94a2e443162cdb
m85091081 <m85091081@gmail.com>
2016-11-30 19:30:08 +0800

    add compute

--- core_module/compute.py
+++ core_module/compute.py
@@ -1,19 +1,25 @@
 import gethtml
+from distributed import Client
+client = Client()
 rawdata = gethtml.dataget()
 
-def mycar():
+def carcomp(me):
+    print(me)
     KL_Mycar_X = int(float(rawdata.get('car0')[1])*1010000)
     KL_Mycar_Y = int(float(rawdata.get('car0')[0])*1050000)
     return [KL_Mycar_X,KL_Mycar_Y]
 
-def othercar():
+def othercar(mycar):
     car = 1
     KL_Othercar_Y = []
     KL_Othercar_X = []
     for x in range(1,5):
-        KL_Othercar_Y.append(int(float(rawdata.get('car'+str(car))[0])*1050000)-int(mycar()[1]))
-        KL_Othercar_X.append(int(float(rawdata.get('car'+str(car))[1])*1010000)-int(mycar()[0]))
+        KL_Othercar_Y.append(int(float(rawdata.get('car'+str(car))[0])*1050000)-int(mycar[1]))
+        KL_Othercar_X.append(int(float(rawdata.get('car'+str(car))[1])*1010000)-int(mycar[0]))
         car = car + 1
     return [KL_Othercar_X,KL_Othercar_Y]
 
+anscar = client.submit(carcomp,'1')
+ans = client.submit(othercar,anscar)
 
+print(ans.result())
